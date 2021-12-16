9f01f26193bc43dae8a828004e7be4b3200ce662
hoangbao177 <96256950+hoangbao177@users.noreply.github.com>
2021-12-17 00:55:52 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 const CONFIG = {
-    titleWeb: "hihii",
-    introTitle: 'Hello Linh',
+    titleWeb: "Tớ là Bảoooo=))",
+    introTitle: 'Hi Thảoo 🤣❤️',
     introDesc: `Trái đất vốn lạ thường
     Mà sao em cứ đi nhầm đường
     Lạc vào tim anh lẻ loi
@@ -14,7 +14,7 @@ const CONFIG = {
     btnReply: 'Gửi cho bạn <3',
     reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
     mess: 'Mình biết mà 🥰. Yêu bạn nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h, mình qua đón đi chơi nha.',
+    messDesc: 'Mãi bên anh em nhíe ❤️.',
     btnAccept: 'Okiiiii lun <3',
-    messLink: 'https://github.com/zukahai/Confess-Crush' //link mess của các bạn. VD: https://m.me/nam.nodemy
+    messLink: 'https://www.facebook.com/hbaoo.0/' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
