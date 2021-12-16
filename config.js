bd736a0d44f3c410b1f276aaea31a08bab004449
hoangbao177 <96256950+hoangbao177@users.noreply.github.com>
2021-12-17 00:27:48 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 const CONFIG = {
-    titleWeb: "Tiêu đề web",
-    introTitle: 'Tên người yêu bạn',
+    titleWeb: "hihii",
+    introTitle: 'Hello Linh',
     introDesc: `Trái đất vốn lạ thường
     Mà sao em cứ đi nhầm đường
     Lạc vào tim anh lẻ loi
